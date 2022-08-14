7214d3f298ba3432d1a4552b3a6d76a0db41a74a
agildojunior <agildo500@hotmail.com>
2022-08-14 16:05:07 -0300

    botao logar adm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,8 @@
                 <input type="password"placeholder="Password"name=""value="">
             </div>
 
-            <a href="/home"><input class="btn" type="button" name="" value="Logar"></a>
+            <a href="/home"><input class="btn" type="button" name="" value="Logar Admin"></a>
+            <a href="/taxistaCorridasDisponiveis"><input class="btn" type="button" name="" value="Logar Taxista"></a>
             <input class="btn2" type="button" name="" value="Cadastro">
 
         </div>
